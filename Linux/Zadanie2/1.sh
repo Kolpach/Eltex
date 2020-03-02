80132d9e5e5bdce5657d1eb60a86c254e6236f6d
Kolpach <null>
2020-03-02 23:33:17 +0700

    added Zad2

---
+++ Linux/Zadanie2/1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Primer while
+directory=$1
+nameOfFolder=0
+nameOfSecondFolder=0
+nameOfFile=0
+while [ $nameOfFolder -lt 50 ]
+do
+mkdir $directory/$nameOfFolder
+while [ $nameOfSecondFolder -lt 100 ]
+do
+mkdir $directory/$nameOfFolder/$nameOfSecondFolder
+while [ $nameOfFile -lt 200 ]
+do
+touch $directory/$nameOfFolder/$nameOfSecondFolder/$nameOfFile
+nameOfFile=$(( $nameOfFile+1 ))
+done
+nameOfFile=0
+nameOfSecondFolder=$(( $nameOfSecondFolder+1 ))
+done
+nameOfSecondFolder=0
+nameOfFolder=$(( $nameOfFolder+1 ))
+done
+
