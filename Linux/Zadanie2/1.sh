1e194aedad30a75b65c1fc099091afb6cbaa83ec
Kolpach <null>
2020-03-02 23:45:39 +0700

    added Zad2.2

--- Linux/Zadanie2/1.sh
+++ Linux/Zadanie2/1.sh
@@ -6,18 +6,18 @@ nameOfSecondFolder=0
 nameOfFile=0
 while [ $nameOfFolder -lt 50 ]
 do
-mkdir $directory/$nameOfFolder
-while [ $nameOfSecondFolder -lt 100 ]
-do
-mkdir $directory/$nameOfFolder/$nameOfSecondFolder
-while [ $nameOfFile -lt 200 ]
-do
-touch $directory/$nameOfFolder/$nameOfSecondFolder/$nameOfFile
-nameOfFile=$(( $nameOfFile+1 ))
-done
-nameOfFile=0
-nameOfSecondFolder=$(( $nameOfSecondFolder+1 ))
-done
+	mkdir $directory/$nameOfFolder
+	while [ $nameOfSecondFolder -lt 100 ]
+	do
+		mkdir $directory/$nameOfFolder/$nameOfSecondFolder
+		while [ $nameOfFile -lt 200 ]
+		do
+			touch $directory/$nameOfFolder/$nameOfSecondFolder/$nameOfFile
+			nameOfFile=$(( $nameOfFile+1 ))
+		done
+		nameOfFile=0
+		nameOfSecondFolder=$(( $nameOfSecondFolder+1 ))
+	done
 nameOfSecondFolder=0
 nameOfFolder=$(( $nameOfFolder+1 ))
 done
--- Linux/Zadanie2/2.sh
+++ Linux/Zadanie2/2.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 # Primer while
 directory=$1
+maxFirstFolders=$2
+maxSecondFolders=$3
+maxFiles=$4
 nameOfFolder=0
 nameOfSecondFolder=0
 nameOfFile=0
-while [ $nameOfFolder -lt 50 ]
+
+while [ $nameOfFolder -lt $maxFirstFolders ]
 do
-mkdir $directory/$nameOfFolder
-while [ $nameOfSecondFolder -lt 100 ]
-do
-mkdir $directory/$nameOfFolder/$nameOfSecondFolder
-while [ $nameOfFile -lt 200 ]
-do
-touch $directory/$nameOfFolder/$nameOfSecondFolder/$nameOfFile
-nameOfFile=$(( $nameOfFile+1 ))
-done
-nameOfFile=0
-nameOfSecondFolder=$(( $nameOfSecondFolder+1 ))
-done
+	mkdir $directory/$nameOfFolder
+	while [ $nameOfSecondFolder -lt $maxSecondFolders ]
+	do
+		mkdir $directory/$nameOfFolder/$nameOfSecondFolder
+		while [ $nameOfFile -lt $maxFiles ]
+		do
+			touch $directory/$nameOfFolder/$nameOfSecondFolder/$nameOfFile
+			nameOfFile=$(( $nameOfFile+1 ))
+		done
+		nameOfFile=0
+		nameOfSecondFolder=$(( $nameOfSecondFolder+1 ))
+	done
 nameOfSecondFolder=0
 nameOfFolder=$(( $nameOfFolder+1 ))
 done
